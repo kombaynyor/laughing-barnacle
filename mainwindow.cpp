742c224769063590ebe3f0d4a86cbac89d5b47a3
kombaynyor <utes12_7@mail.ru>
2021-10-22 12:30:14 +0300

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,30 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QtSql>
+QSqlDatabase db;
+QSqlTableModel * model;
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    db=QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("basse");
+    if(db.open()){
+        qDebug() << "open";
+    }
+    model = new QSqlTableModel();
+    QSqlQuery qwe;
+    //qwe.exec("CREATE TABLE PROBA(id PRIMARY KEY NOT NULL, pole VARCHAR(40),nepole INT);");
+    model->setTable("tybl");
+    model->select();
+    ui->tableView->setModel(model);
+    //qwe.exec("INSERT INTO tybl VALUES(1,'wew','4')");
+    qDebug() << qwe.lastError().text();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
